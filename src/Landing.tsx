acb8fa30ed8371f0cbbf4d65b288c7a4cdd36ec3
yottayocta <chubbean.rq@gmail.com>
2025-07-14 01:11:21 -0400

    feat: feature images and captions

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -24,18 +24,20 @@ export function Landing({ handleLoaded }: LandingProps) {
         </nav>
       </div>
 
-      <div className="flex-grow flex flex-col items-center justify-center gap-8">
-        <h1 className="text-3xl text-neutral-800 text-center max-w-2xl">
-          Study Music Without Interruption
-        </h1>
-        <LoadButton
-          handleLoaded={handleLoaded}
-          buttonText="Load a Recording to Get Started"
-        ></LoadButton>
+      <div className="flex-grow flex flex-col items-center justify-center gap-16">
+        <div className="flex flex-col gap-8 items-center justify-center">
+          <h1 className="text-3xl text-neutral-800 text-center max-w-2xl">
+            Study Music Without Interruption
+          </h1>
+          <LoadButton
+            handleLoaded={handleLoaded}
+            buttonText="Load a Recording to Get Started"
+          ></LoadButton>
+        </div>
         <img
           src="/demoImage.png"
           alt="Demo"
-          className="max-w-2xl h-auto mt-8 shadow-md"
+          className="max-w-3xl h-auto mt-8 shadow-md"
         />
       </div>
 
@@ -48,6 +50,61 @@ export function Landing({ handleLoaded }: LandingProps) {
         <div className="flex flex-col items-center justify-center gap-1 text-neutral-600">
           <p>An open-source recording looper and analysis app,</p>
           <p>designed to help you replay and analyze recordings seamlessly</p>
+        </div>
+
+        <div className="flex justify-center items-center gap-16 mt-8">
+          <div className="flex flex-col items-center gap-2 max-w-min">
+            <img
+              src="/controls.png"
+              alt="Fine-grain playback playback control"
+              className="min-w-72 h-48 object-contain border border-neutral-2 rounded-xs"
+            />
+            <p className="text-center text-neutral-600">
+              Fine-grain playback control
+            </p>
+          </div>
+          <div className="flex flex-col items-center gap-2 max-w-min">
+            <img
+              src="/loopfreeze.png"
+              alt="Freeze and automatically loop audio"
+              className="min-w-72 h-48 object-contain border border-neutral-2 rounded-xs"
+            />
+            <p className="text-center text-neutral-600">
+              Freeze and automatically loop audio
+            </p>
+          </div>
+          <div className="flex flex-col items-center gap-2 max-w-min">
+            <img
+              src="/frequencies.png"
+              alt="Visualize frequencies in real time"
+              className="min-w-72 h-48 object-contain border border-neutral-2 rounded-xs"
+            />
+            <p className="text-center text-neutral-600">
+              Visualize frequencies in real time
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-16 mt-8">
+          <div className="flex flex-col items-center gap-2 max-w-min">
+            <img
+              src="/filetype.png"
+              alt="Supports audio and video files"
+              className="min-w-72 h-48 object-contain border border-neutral-2 rounded-xs"
+            />
+            <p className="text-center text-neutral-600">
+              Supports audio files <em>and</em> video files
+            </p>
+          </div>
+          <div className="flex flex-col items-center gap-2 max-w-min">
+            <img
+              src="/shortcuts.png"
+              alt="Shortcuts for everything"
+              className="min-w-72 h-48 object-contain border border-neutral-2 rounded-xs"
+            />
+            <p className="text-center text-neutral-600">
+              Shortcuts for <em>everything</em>
+            </p>
+          </div>
         </div>
       </div>
 
