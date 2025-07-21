be4134f9a6ae43c28771a839e2855cd293ee76e5
yottayocta <chubbean.rq@gmail.com>
2025-07-21 16:58:43 -0400

    task: pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,40 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate", // auto updates service worker
+      includeAssets: ["favicon/favicon.svg", "favicon/apple-touch-icon.png"],
+      manifest: {
+        name: "GreenDolphin",
+        short_name: "GreenDolphin",
+        description: "Music looper and analysis tool",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "favicon/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "favicon/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "favicon/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
