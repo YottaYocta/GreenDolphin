d2718d36df6507de1409317b648979d2be57ad43
yottayocta <chubbean.rq@gmail.com>
2025-08-07 12:54:01 -0500

    task: faq on landing page

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -24,6 +24,18 @@ export function Landing({ handleLoaded }: LandingProps) {
         Features
       </a>,
       <a
+        href="#faq-section"
+        className="text-neutral-600 hover:text-emerald-600 text-nowrap"
+        onClick={(e) => {
+          e.preventDefault();
+          document.getElementById("features-section")?.scrollIntoView({
+            behavior: "smooth",
+          });
+        }}
+      >
+        FAQs
+      </a>,
+      <a
         href="#report-bug-section"
         className="text-neutral-600 hover:text-emerald-600 text-nowrap"
         onClick={(e) => {
@@ -193,58 +205,95 @@ export function Landing({ handleLoaded }: LandingProps) {
           </div>
         </div>
 
-        <div className="flex flex-wrap items-start w-full max-w-2xl justify-between gap-24">
-          <section
-            id="report-bug-section"
-            className="w-full flex flex-col justify-between gap-8"
-          >
-            <h2 className="text-lg text-neutral-400  max-w-2xl w-full border-b border-neutral-2">
-              Encounter a Bug? Have Questions?
-            </h2>
-            <div className="flex flex-wrap gap-2 text-neutral-600 items-center">
-              <p>Please fill out</p>
+        <section
+          className="flex flex-wrap items-start w-full max-w-2xl justify-between gap-8"
+          id="faq-section"
+        >
+          <h2 className="text-lg text-neutral-400  max-w-2xl w-full border-b border-neutral-2">
+            FAQs
+          </h2>
+
+          <div className="flex flex-col w-full gap-2">
+            <h3 className="text-neutral-600">
+              Does GreenDolphin convert recordings to sheet music automatically?
+            </h3>
+            <span className="text-neutral-400 flex flex-col gap-2">
+              <p>No, GreenDolphin is a tool to help you do that yourself :)</p>
+              <p>
+                Learning music by ear is an invaluable skill for any musician,
+                and automatic transcription (while convenient) takes away the
+                practice you get from closely studying a recording.
+              </p>
+            </span>
+          </div>
+          <div className="flex flex-col w-full gap-2">
+            <h3 className="text-neutral-600">
+              How does the frequency visualization work?
+            </h3>
+            <span className="text-neutral-400 flex flex-col gap-2">
+              <p>
+                The frequency visualization uses Fast Fourier Transform (FFT) to
+                estimate which 'notes' are being played at any given time.
+                Likely pitches are the highest peaks above the piano.
+              </p>
+              <p>
+                Freezing the playback may be helpful in seeing the notes of a
+                chord
+              </p>
+            </span>
+          </div>
+        </section>
+
+        <section
+          id="report-bug-section"
+          className="w-full max-w-2xl flex flex-col justify-between gap-8"
+        >
+          <h2 className="text-lg text-neutral-400  max-w-2xl w-full border-b border-neutral-2">
+            Encounter a Bug? Have Feedback?
+          </h2>
+          <div className="flex flex-wrap gap-2 text-neutral-600 items-center">
+            <p>Please fill out</p>
+            <Button
+              className="border border-emerald-500 rounded-full bg-emerald-50 text-emerald-700 hover:bg-emerald-100"
+              text="This Google Form"
+              onClick={() =>
+                window.open("https://forms.gle/XmVubjrPpvwwXJ7U7", "_blank")
+              }
+            />
+            <p>or contact me</p>
+            <a href="mailto:rwq3@cornell.edu">
               <Button
                 className="border border-emerald-500 rounded-full bg-emerald-50 text-emerald-700 hover:bg-emerald-100"
-                text="This Google Form"
+                text="Via Email"
+              />
+            </a>
+          </div>
+        </section>
+
+        <section
+          id="support-section"
+          className="w-full max-w-2xl gap-8 flex flex-col justify-between"
+        >
+          <h2 className="text-lg text-neutral-400 max-w-2xl w-full border-b border-neutral-2">
+            Like What You See?
+          </h2>
+          <div className="flex flex-wrap gap-1 text-neutral-600 items-center gap-y-2">
+            <p>Consider sharing GreenDolphin</p>
+            <span className="flex items-center gap-2">
+              with friends or
+              <Button
+                className="border border-emerald-500 rounded-full bg-emerald-50 text-emerald-700 hover:bg-emerald-100"
+                text="Starring the GitHub"
                 onClick={() =>
-                  window.open("https://forms.gle/XmVubjrPpvwwXJ7U7", "_blank")
+                  window.open(
+                    "https://github.com/YottaYocta/GreenDolphin",
+                    "_blank"
+                  )
                 }
               />
-              <p>or contact me</p>
-              <a href="mailto:rwq3@cornell.edu">
-                <Button
-                  className="border border-emerald-500 rounded-full bg-emerald-50 text-emerald-700 hover:bg-emerald-100"
-                  text="Via Email"
-                />
-              </a>
-            </div>
-          </section>
-
-          <section
-            id="support-section"
-            className="w-full gap-8 flex flex-col justify-between"
-          >
-            <h2 className="text-lg text-neutral-400 max-w-2xl w-full border-b border-neutral-2">
-              Like What You See?
-            </h2>
-            <div className="flex flex-wrap gap-1 text-neutral-600 items-center gap-y-2">
-              <p>Consider sharing GreenDolphin</p>
-              <span className="flex items-center gap-2">
-                with friends or
-                <Button
-                  className="border border-emerald-500 rounded-full bg-emerald-50 text-emerald-700 hover:bg-emerald-100"
-                  text="Starring the GitHub"
-                  onClick={() =>
-                    window.open(
-                      "https://github.com/YottaYocta/GreenDolphin",
-                      "_blank"
-                    )
-                  }
-                />
-              </span>
-            </div>
-          </section>
-        </div>
+            </span>
+          </div>
+        </section>
         <div className="w-full flex items-center justify-center">
           <p className="text-neutral-400">
             Source code licensed under <em>GPL 3</em>
